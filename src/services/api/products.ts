7f90dac5037251670c93714c6a78a2bfd1f408c3
Vitor Berto <vitorberto.dev@gmail.com>
2024-05-11 21:37:22 -0300

    fix: environment base url

--- src/services/api/products.ts
+++ src/services/api/products.ts
@@ -9,7 +9,7 @@ export async function getProducts(
   options = { page: 1, rows: 8 }
 ): Promise<IResponse> {
   const result = await fetch(
-    `https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=${options.page}&rows=${options.rows}&sortBy=id&orderBy=DESC`
+    `${process.env.NEXT_PUBLIC_BASE_URL}/products?page=${options.page}&rows=${options.rows}&sortBy=id&orderBy=DESC`
   );
 
   return result.json();
